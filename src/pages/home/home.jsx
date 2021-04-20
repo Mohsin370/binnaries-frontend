d51b2f2e494963c9e1906f454ca98a94fcd0604b
mohsin370 <mohsinijaz13@gmail.com>
2021-04-20 22:49:51 +0500

    home screen minor updated

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import styles from './home.module.css'
+import styles from "./home.module.css";
 
 function Home() {
-  return <div className={styles.test}>Home Page</div>;
+  return <div className={styles.test}>Home</div>;
 }
 
 export default Home;
