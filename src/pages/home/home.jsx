c1b231e63c2731ef91c7b2ff0a39c9a6e552f0ee
mohsin <mohsinijaz13@gmail.com>
2021-04-22 04:16:36 +0500

    navbar seprated from main

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import styles from "./home.module.css";
+import Navbar from '../../components/navbar/navbar'
 
 function Home() {
-  return <div className={styles.test}>Home</div>;
+  return(
+  <div>
+    <Navbar></Navbar>
+    <div className={styles.test}>Home</div>
+  </div>
+  )
 }
 
 export default Home;
