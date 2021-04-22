9f4e2553e41b28f4085f6d6339b54fb882a7e56b
mohsin <mohsinijaz13@gmail.com>
2021-04-22 05:09:33 +0500

    bootstrap installed navbar basic design added

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+
 function Navbar() {
-  return <div>This is Navbar</div>;
+  return( 
+  <div>
+    <div className="d-flex justify-content-between p-4">
+      <div>Logo</div>
+      <div>
+        <ul className="d-flex list-unstyled">
+          <li className="pl-3">ListItems</li>
+          <li className="pl-3">ListItems</li>
+          <li className="pl-3">ListItems</li>
+          <li className="pl-3">ListItems</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  )
 }
 
 export default Navbar;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./home.module.css";
-import Navbar from '../../components/navbar/navbar'
+import Navbar from '../../components/navbar/navbar';
+import HomeSVG from '../../assets/images/home.svg'
 
 function Home() {
   return(
-  <div>
+  <div className={styles.main}>
     <Navbar></Navbar>
     <div className={styles.test}>Home</div>
+    <img src={HomeSVG} className={styles.HomeSvg}/>
   </div>
   )
 }
