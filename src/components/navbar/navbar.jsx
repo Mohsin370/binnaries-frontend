27693ecf8750504528b977ca5021b60c2c459ada
mohsin <mohsinijaz13@gmail.com>
2021-04-22 22:29:10 +0500

    front page 90% with animations

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,21 +1,20 @@
 import React from "react";
-
+import styles from './navbar.module.css';
+import { Link } from 'react-router-dom'
 
 function Navbar() {
-  return( 
-  <div>
-    <div className="d-flex justify-content-between p-4">
-      <div>Logo</div>
-      <div>
-        <ul className="d-flex list-unstyled">
-          <li className="pl-3">ListItems</li>
-          <li className="pl-3">ListItems</li>
-          <li className="pl-3">ListItems</li>
-          <li className="pl-3">ListItems</li>
-        </ul>
+  return (
+    <div>
+      <div className="d-flex justify-content-between p-4 pt-5">
+        <div type="button" className={[styles.navbar_font, styles.nav_logo_animation, "ml-5 cursor-pointer"].join(' ')} >Binaries</div>
+        <div>
+          <ul className={[styles.nav_links_animation, "d-flex list-unstyled pr-5"].join(' ')}>
+            <Link to="/dashboard" className={styles.home_links}>Dashboard</Link>
+            <Link to="/dashboard" className={[styles.home_links, "ml-4"].join(' ')}>Login</Link>
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
   )
 }
 
