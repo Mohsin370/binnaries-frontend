b29b2abe79f331f0d18ada0d1990c643c6cae762
mohsin370 <mohsinijaz13@gmail.com>
2021-04-25 04:25:07 +0500

    home page little updates

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -8,18 +8,35 @@ function Home() {
   return (
     <div className={styles.main}>
       <Navbar></Navbar>
-      <div className="row m-auto p-5">
-        <div className={ [styles.animation_main, "col-md-6 text-center"].join(" ")}>
+      <div className="row m-auto pl-5">
+        <div className={[styles.animation_main, "col-md-5 text-center pt-4"].join(" ")}>
           <div className={styles.landing_text}>Grow & Manage Business</div>
           <h4 className="m-auto w-75 pt-5">Binaries is a platform where you can manage your business
-          <br></br>
           deals, records and your documents. Let's make the
-          <br></br>
           business in flow. </h4>
           <Button type="button" className="btn btn-primary mt-5 btn-lg">Get Started</Button>
         </div>
-        <div className="col-md-6 d-none d-sm-block">
+        <div className="col-md-7 d-none d-sm-block text-center">
           <img src={HomeSVG} className={styles.HomeSvg} alt="business Logo" />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-3 mr-4">
+          <div>
+
+          </div>
+        </div>
+
+        <div class="col-md-3 mr-4">
+          <div>
+
+          </div>
+        </div>
+
+        <div class="col-md-3 mr-4">
+          <div>
+
+          </div>
         </div>
       </div>
     </div>
