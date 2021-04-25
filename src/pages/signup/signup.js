5100a7daa274c9bd72bcc244f9c0539982bfff52
mohsin370 <mohsinijaz13@gmail.com>
2021-04-25 22:54:23 +0500

    sign up page design updated

--- src/pages/signup/signup.js
+++ src/pages/signup/signup.js
@@ -2,8 +2,7 @@ import React from 'react';
 import styles from './signup.module.css';
 import { Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
-import SignUpImage from '../../assets/images/signup.jpg';
-import HomeSVG from '../../assets/images/home.svg';
+import SignUpImage from '../../assets/images/signup.svg';
 
 
 const Signup = () => {
@@ -13,13 +12,15 @@ const Signup = () => {
                 <div className="w-75 h-75 m-auto bg-white">
                     <div className="row m-auto h-100">
                         <div className="col-md-4 h-100" style= {{backgroundColor : "#168574"}}>
-                            <h3 className="pt-5">Binaries</h3>
-                            <img src={HomeSVG} className={styles.signupImg} />
+                        <h5 className="pt-4 pl-2 text-white">Binaries</h5>
+
+                            <h3 className="pt-5 pl-2 pb-4 text-white">A few clicks away from managing your businesses.</h3>
+                            <img src={SignUpImage} className={styles.signupImg} alt="signup" />
                         </div>
                         <div className="col-md-8 h-100 d-flex align-items-center">
-                            <div className="p-5 h-75 w-100 ">
+                            <div className="pl-5 h-75 w-100 ">
                                 <h3><b>Register</b></h3>
-                                <h5 className="pt-5"><b>Manage all your records effeciently</b></h5>
+                                <h5 className="pt-5"><b>Manage all your records efficiently</b></h5>
                                 <h6 className="w-75 text-secondary">Let's get you all setup so you can verify your personal account and begin setting up your profile</h6>
                                 <hr></hr>
                                 <form>
@@ -49,14 +50,14 @@ const Signup = () => {
 
                                         <div className="col-md-12 pt-4 mb-5">
                                             <input className="" type="checkbox" />
-                                            <span className="text-secondary pl-3">I agree to all the Terms and Conditions</span>
+                                            <span className="text-secondary pl-3">I agree to all the <span className="text-primary font-weight-bold">Terms and Conditions</span></span>
                                         </div>
 
                                     </div>
                                     <div className="m-auto">
                                         <Button className="pl-3 pr-3"> Create Account</Button>
                                         <h6 className="pt-3">
-                                            <b>Already have an account?<Link to="/login" className="pl-2">Login</Link></b>
+                                            <b>Already have an account?<Link to="/login" className="text-decoration-none pl-2">Login</Link></b>
                                         </h6>
                                     </div>
 
