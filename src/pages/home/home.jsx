154439f7572b507f3bf7d091e4f25bda1bb58821
mohsin <mohsinijaz13@gmail.com>
2021-04-26 00:14:13 +0500

    home page icons added

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import styles from "./home.module.css";
 import Navbar from '../../components/navbar/navbar';
 import HomeSVG from '../../assets/images/home.svg';
-import { Button } from 'react-bootstrap'
+import { Button } from 'react-bootstrap';
+import fbIcon from '../../assets/icons/fb.svg';
+import LinkedinIcon from '../../assets/icons/linkedin.svg';
+import TwitterIcon from '../../assets/icons/twitter.svg';
 
 function Home() {
   return (
@@ -19,6 +22,12 @@ function Home() {
         <div className="col-md-8 d-none d-sm-block text-center">
           <img src={HomeSVG} className={styles.HomeSvg} alt="business Logo" />
         </div>
+      </div>
+      <div className="text-center p-4">
+        <h5>Catch us on Social Media:</h5>
+       <a href="https://www.facebook.com/khawaja.mohsin1/" target="_blank" without rel="noreferrer"> <img src={fbIcon} alt="fb"/> </a>
+       <a href="https://www.linkedin.com/in/khawaja-mohsin-9a5b75125/" target="_blank" without rel="noreferrer"><img src={LinkedinIcon} alt="linkedin"/></a>
+       <a href="https://twitter.com/mohsinijaz13" target="_blank" without rel="noreferrer"><img src={TwitterIcon} alt="Twitter"/></a>
       </div>
     </div>
   )
