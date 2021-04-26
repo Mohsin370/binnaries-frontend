cd744bebaa3e5e47fb839eeff5aeeaf20195d6ba
mohsin <mohsinijaz13@gmail.com>
2021-04-27 00:30:14 +0500

    side bar design added

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-// import styles from './dashboard.module.css';
-import { connect } from 'react-redux'
-import {addCounter} from '../../redux/actions/actions'
+import styles from './dashboard.module.css';
+import { connect } from 'react-redux';
+import { addCounter } from '../../redux/actions/actions';
+import SideNav from '../../components/dashboard_Nav/sideNav'
 
 function Dashboard(props) {
 
@@ -11,9 +12,16 @@ function Dashboard(props) {
   }
 
   return (
-    <div className="text-center mt-5">
-      <div>{props.loginReducer.counter}</div>
-      <button className="mt-5" onClick={updateCounter}>Add Counter</button>
+    <div className={styles.dashboardMain}>
+      <div className="row m-auto h-100">
+        <div className="col-md-3 col-xl-2 p-0" style={{backgroundColor:"#0d2e70"}}>
+          <SideNav></SideNav>
+        </div>
+        <div className="col-md-9 col-xl-10">
+          <div>{props.loginReducer.counter}</div>
+          <button className="mt-5" onClick={updateCounter}>Add Counter</button>
+        </div>
+      </div>
     </div>
   )
 
