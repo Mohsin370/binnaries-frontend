45b9da2d247d959399841d75b0633241e64db059
mohsin <mohsinijaz13@gmail.com>
2021-06-27 01:02:33 +0500

    env fiole changed to right project

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -2,7 +2,7 @@ const Base_url_server = () => {
     let BaseURL = "http://localhost:4000";
   
     if (process.env.REACT_APP_SERVER === "production") {
-      return (BaseURL = "https://ebh-backend-staging.herokuapp.com");
+      return (BaseURL = "https://binaries-backend.herokuapp.com");
     } else {
       return BaseURL;
     }
