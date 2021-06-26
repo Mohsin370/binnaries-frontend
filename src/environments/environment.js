355267ef3f6e7aef14ca9bd44506ade48c1dbf8b
mohsin <mohsinijaz13@gmail.com>
2021-06-27 00:37:52 +0500

    front end evironment updated for deployment

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,3 +1,13 @@
-export const environment = {
-    BaseURL : "http://localhost:4000"
-}
+const Base_url_server = () => {
+    let BaseURL = "http://localhost:4000";
+  
+    if (process.env.REACT_APP_SERVER === "production") {
+      return (BaseURL = "https://ebh-backend-staging.herokuapp.com");
+    } else {
+      return BaseURL;
+    }
+  };
+  
+  export const environment = {
+    BaseURL: Base_url_server(),
+  };
