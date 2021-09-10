5892f654cc43422310ae6cf5c256400f2db89dac
mohsin370 <mohsinijaz13@gmail.com>
2021-09-10 21:36:57 +0500

    c data table added in customers table

--- src/pages/dashboardPages/customers/customers.jsx
+++ src/pages/dashboardPages/customers/customers.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import DashboardHOC from '../dashboardHOC';
+import DataTable, { createTheme } from 'react-data-table-component-with-filter';
+
+
+const data = [{ id: 1, title: 'Conan the Barbarian', year: '1982' },
+{ id: 2, title: 'Bonan the Barbarian', year: '1983' }];
+const columns = [
+  {
+    name: 'Title',
+    selector: 'title',
+    sortable: true,
+		filterable: true,
+  },
+  {
+    name: 'Year',
+    selector: 'year',
+    sortable: true,
+    right: false,
+  },
+];
 
 function Customers() {
     return (
         <div>
-            customers
+              <DataTable
+                    title="Arnold Movies"
+                    columns ={columns}
+                    data ={data}
+                    pagination
+                />
         </div>
     )
 }
